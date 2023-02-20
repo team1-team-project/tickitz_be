570eb3af16a218e85f4d2a13c835c599e0249fd8
Yasfiq <mohammadyasfiq31@gmail.com>
2023-02-21 01:02:40 +0700

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 5000;
 
 //import internal
 
@@ -25,7 +25,7 @@ app.use(json());
 app.use(cors()); //semua bisa akses
 
 //routes parent
-app.use("https://tickitz.herokuapp.com/api/", router);
+app.use("/api/", router);
 
 //endpoint
 //routing if can't get a routes
