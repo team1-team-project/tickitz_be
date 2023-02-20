fbe446246f39bef06913ab8c28e01582676306d0
Yasfiq <mohammadyasfiq31@gmail.com>
2023-02-21 01:14:02 +0700

    add script npm start

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const { PORT } = process.env;
 
 //import internal
 
