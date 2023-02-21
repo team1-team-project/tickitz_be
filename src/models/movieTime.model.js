e152e4cedb12315f355026969e0a488c86fb3a10
Yasfiq <mohammadyasfiq31@gmail.com>
2023-02-21 18:15:58 +0700

    fix bug movie time

--- src/models/movieTime.model.js
+++ src/models/movieTime.model.js
@@ -5,14 +5,14 @@ const movieTimeModel = {
   getMovieTime: (id, id_cinema) => {
     return new Promise((success, failed) => {
       db.query(
-        "SELECT t.id_time FROM time AS t INNER JOIN data_movies AS d ON d.id_movies=$1 AND d.id_time=t.id_time AND d.id_room=$2",
+        "SELECT t.start_time,t.id_time FROM time AS t INNER JOIN data_movies AS d ON d.id_movies=$1 AND d.id_time=t.id_time AND d.id_room=$2",
         [id, id_cinema],
         (error, result) => {
           if (error) return failed(error.message);
           if (result.rows.length === 0) return failed("Id not found!");
           const hasil = [];
           result.rows.map((item) => {
-            hasil.push(item.start_time);
+            hasil.push({ start_time: item.start_time, id_time: item.id_time });
           });
           return success(hasil);
         }
