83452db3cf0bdcc143ea3dffbb0a44391f1026a2
Yasfiq <mohammadyasfiq31@gmail.com>
2023-02-21 22:59:58 +0700

    add cinema logo

--- src/middlewares/formUpload.js
+++ src/middlewares/formUpload.js
@@ -24,8 +24,25 @@ const storageOnline = new CloudinaryStorage({
   },
 });
 
-const formUpload = multer({
-  storage: storage,
+// const formUpload = multer({
+//   storage: storage,
+//   fileFilter: (req, file, cb) => {
+//     //console.log(file);
+//     let formatType = path.extname(file.originalname);
+//     if (formatType == ".png" || formatType == ".jpg" || formatType == ".jpeg") {
+//       cb(null, true);
+//     } else {
+//       cb("image not valid", false);
+//     }
+//   },
+//   limits: {
+//     fileSize: 1048576 * 2, //2 mb
+//   },
+//   //dest: './public/data/uploads/'
+// });
+
+const formUploadOnline = multer({
+  storage: storageOnline, //test bisa atau ga
   fileFilter: (req, file, cb) => {
     //console.log(file);
     let formatType = path.extname(file.originalname);
@@ -41,22 +58,5 @@ const formUpload = multer({
   //dest: './public/data/uploads/'
 });
 
-/*const formUploadOnline = multer({
-  storage: storageOnline, //test bisa atau ga
-  fileFilter: (req, file, cb) => {
-    //console.log(file);
-    let formatType = path.extname(file.originalname);
-    if (formatType == ".png" || formatType == ".jpg" || formatType == ".jpeg") {
-      cb(null, true);
-    } else {
-      cb("image not valid", false);
-    }
-  },
-  limits: {
-    fileSize: 1048576 2, //2 mb
-  },
-  //dest: './public/data/uploads/'
-});*/
-
-module.exports = formUpload;
-//module.exports = formUploadOnline;
+// module.exports = formUpload;
+module.exports = formUploadOnline;
